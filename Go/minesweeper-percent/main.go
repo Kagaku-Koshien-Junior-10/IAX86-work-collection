690134179a0430780782a87f10846759d51523f2
IAX86 <grapeia07na86@gmail.com>
2023-01-04 21:02:28 +0900

    Update main.go

--- Go/minesweeper-percent/main.go
+++ Go/minesweeper-percent/main.go
@@ -2,22 +2,22 @@ package main
 
 import "fmt"
 
-n := 36
-b := 5
-i := n - b
-d := i / n
-c := 0
-e := 0
-nc := 0
-ic := 0
+var n float64 = 9
+var b float64 = 3
+var i float64 = n - b
+var d float64 = i / n
+var c float64 = 0
+var e float64 = 0
+var nc float64 = 0
+var ic float64 = 0
 
 func main() {
-	for c != i {
-		c = c++
+	for c < i-1 {
+		c = c + 1
 		nc = n - c
 		ic = i - c
 		e = ic / nc
 		d = d * e
 	}
-	println(d)
+	fmt.Println("Answer is", d, "?")
 }
